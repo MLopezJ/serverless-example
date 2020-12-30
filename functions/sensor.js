787e36b6108df641216d0d5d7c737fe899475c77
Mauro Lopez Jimenez <mlopez-as@SCA-LP374.local>
2020-12-30 16:48:17 -0600

    feature: requet serverless fuction by http

--- functions/sensor.js
+++ functions/sensor.js
@@ -1,8 +1,39 @@
 const sensordData = require("../data/dummy.json")
+const { URL } = require("url");
+const { fetch } = require("node-fetch");
+const { query } = require("./util/hasura");
 
-exports.handler = async () => {
-    return{
-        statusCode: 200,
-        body: JSON.stringify(sensordData)
-    };
+exports.handler = async ({queryStringParameters}) => {
+  const { id } = queryStringParameters;
+
+  let response = {
+    statusCode: 400,
+    body: JSON.stringify({message:"not found"})
+  };
+
+  if (id) {
+    // get all the records from the sensors who belong to sample with custom id
+    const graphQuery = `
+      query {
+        sensor(where: {sampleId: {_eq: "${id}"}}) {
+          records {
+            record
+            timestamp
+          }
+          name
+          id
+        }
+      }
+    `
+    const result = await query({
+        query: graphQuery
+    });
+
+    response = {
+      statusCode: 200,
+      body: JSON.stringify(result)
+    }
+  }
+
+  return response;
 };
